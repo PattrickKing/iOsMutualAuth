c9c3b0f19df58778c0a8bdf851545eab334bb578
Patrick W. King <PattrickKing@gmail.com>
2013-11-27 18:19:16 -0600

    running pod install again, adding a subclass for the RestKit RKHTTPRequestOperation object

---
+++ MutualAuthTestHarness/BZHTTPRequestOperation.h
@@ -0,0 +1,13 @@
+//
+//  BZHTTPRequestOperation.h
+//  MutualAuthTestHarness
+//
+//  Created by Patrick King on 11/27/13.
+//  Copyright (c) 2013 Patrick King Consulting LLC. All rights reserved.
+//
+
+#import "RKHTTPRequestOperation.h"
+
+@interface BZHTTPRequestOperation : RKHTTPRequestOperation
+
+@end
---
+++ MutualAuthTestHarness/BZHTTPRequestOperation.m
@@ -0,0 +1,13 @@
+//
+//  BZHTTPRequestOperation.m
+//  MutualAuthTestHarness
+//
+//  Created by Patrick King on 11/27/13.
+//  Copyright (c) 2013 Patrick King Consulting LLC. All rights reserved.
+//
+
+#import "BZHTTPRequestOperation.h"
+
+@implementation BZHTTPRequestOperation
+
+@end
